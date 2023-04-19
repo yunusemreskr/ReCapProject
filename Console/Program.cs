3a185c650818580dbcc5016d03fa839421b43f78
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-04-19 22:13:55 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager= new ProductManager(new InMemoryProductDal());
+        foreach (Car car in productManager.GetAll())
+        {
+            Console.WriteLine(car.Description+ " : " +car.DailyPrice + " TL");
+        }
+
+        IProductDal productDal = new InMemoryProductDal();
+
+        Console.WriteLine("----------------------------------------------------");
+
+        productDal.GetById(3).ForEach(car => Console.WriteLine("Model Açıklaması: " + car.Description));
+
+        Console.WriteLine("----------------------------------------------------");
+
+        productDal.GetAll().ForEach(car => Console.WriteLine(car.Description));
+
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Car> _car;
+        public InMemoryProductDal() 
+        {
+            _car = new List<Car>
+            {
+                new Car {Id=1 , BrandId=1, ColorId=1, DailyPrice=950000, ModelYear=2022, Description="Bmw i20"},
+                new Car {Id=2 , BrandId=5, ColorId=2, DailyPrice=850000, ModelYear=2023, Description="Golf "},
+                new Car {Id=3 , BrandId=4, ColorId=4, DailyPrice=550000, ModelYear=2020, Description="Mercedes Sl"},
+                new Car {Id=4 , BrandId=3, ColorId=3, DailyPrice=750000, ModelYear=2023, Description="Togg"},
+            };
+        
+        }
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(p => p.Id == car.Id);
+            _car.Remove(carToUpdate);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _car.Where(p=> p.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(p=> p.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}
