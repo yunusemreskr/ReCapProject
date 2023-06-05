ecdc4d5eb66c4ead9aaf64e94c2d6493bcd2d475
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-06-05 13:45:52 +0300

    Updated

---
+++ Business/ValidationRules/FluentValidation/UserValidation.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidation: AbstractValidator<User>
+    {
+        public UserValidation()
+        {
+            RuleFor(p => p.Id).NotEmpty();
+            RuleFor(p=>p.FirstName).NotEmpty();
+            RuleFor(p=>p.Email).NotEmpty();
+            RuleFor(p=>p.Password).NotEmpty();
+            RuleFor(p=>p.Password).MinimumLength(6);
+        }
+    }
+}
